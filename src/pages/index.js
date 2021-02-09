3545dfb5bb7ece1386c2a13a4f52db00714b000d
Daniel-Simoes <daniel.simoes@hotmail.com>
2021-02-09 21:30:06 +0000

    TabBar component.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Button } from '@material-ui/core';
 
+import Layout from "../components/layout"
+
 export default function Home() {
   return (
-    <div>
+    <Layout title="YouTube">
       Clone Youtube with Next.js and Material Ui!
       <Button variant="outlined" color="secondary">
         Secondary
       </Button>
-    </div>
+    </Layout>
   )
 } 
---
+++ src/pages/test.js
@@ -0,0 +1,5 @@
+function Test() {
+  return <h1>Just test!</h1>
+}
+
+export default Test;
