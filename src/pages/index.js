ba466cf7c155ccb613aae47a8f96de46aa0ab1e5
Daniel-Simoes <daniel.simoes@hotmail.com>
2021-02-09 20:27:46 +0000

    Material-Ui configured.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { Button } from '@material-ui/core';
+
 export default function Home() {
   return (
     <div>
-       Clone Youtube with Next.js and Material Ui!
+      Clone Youtube with Next.js and Material Ui!
+      <Button variant="outlined" color="secondary">
+        Secondary
+      </Button>
     </div>
   )
 } 
